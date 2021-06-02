f5849eef9842c9da81bd1a69f78774f072896739
Centeno448 <diego.centeno@udla.edu.ec>
2021-06-01 19:28:03 -0500

    fixed tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,16 +1,12 @@
 import { shallowMount } from "@vue/test-utils";
 import App from "@/App.vue";
 
-test('App.vue muestra el mensaje retornado de la función data | Asegúrate de que App.vue defina en su función data la propiedad "message" y que esta sea mostrada dentro de un <p> en el template', () => {
+test('App.vue muestra el mensaje retornado de la función data | Asegúrate de que App.vue defina en su función data la propiedad "message" y que esta sea mostrada dentro de un <p> en el template', async () => {
   const message = "test message";
 
-  const wrapper = shallowMount(App, {
-    data() {
-      return {
-        message
-      };
-    }
-  });
+  const wrapper = shallowMount(App);
+
+  await wrapper.setData({ message });
 
   const p = wrapper.get("p");
 
